b8d1e2ec97ec7f7e7f26dc1b01ed6684c221e83e
Joshua Lucas <joshuamlucas91@gmail.com>
2021-09-29 08:56:07 -0400

    set up React project

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Layout from "./layout/Layout";
+
+function App() {
+  return (
+    <>
+      <Layout>
+        <h1>This is the Dashboard App</h1>
+      </Layout>
+    </>
+  );
+}
+
+export default App;
