6c36ca3c241de72d81c15894c9e698b0bb9fc6aa
Joshua Lucas <joshuamlucas91@gmail.com>
2021-09-29 13:06:31 -0400

    build: configure colors for brand

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
+const colors = require("tailwindcss/colors");
 module.exports = {
   mode: "jit",
   purge: ["./src/**/*.html", "./src/**/*.jsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      // Build your palette here
+      transparent: "transparent",
+      current: "currentColor",
+      gray: colors.blueGray,
+      red: colors.red,
+      green: colors.emerald,
+      cyan: colors.cyan,
+      blue: colors.sky,
+      purple: colors.purple,
+      fuchsia: colors.fuchsia,
+      yellow: colors.amber,
+    },
     extend: {},
   },
   variants: {
