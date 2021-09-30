2134d5f2b3cf9df259e6bb84a3e1860279e2edc1
Joshua Lucas <joshuamlucas91@gmail.com>
2021-09-30 16:22:25 -0400

    style(fileStructure): moved components into a base component directory

--- src/components/KpiGrid.jsx
+++
@@ -1,5 +0,0 @@
-function KpiGrid({ children }) {
-  return <div className="grid grid-cols-2 gap-4 ">{children}</div>;
-}
-
-export default KpiGrid;
---
+++ src/components/base/KpiGrid.jsx
@@ -0,0 +1,5 @@
+function KpiGrid({ children }) {
+  return <section className="grid grid-cols-2 gap-4 ">{children}</section>;
+}
+
+export default KpiGrid;
